42ba10291c8337042c02903af37c74da654d45ae
Andrejrey <102960964+Andrejrey@users.noreply.github.com>
2023-07-14 10:51:54 +0200

    add api fetch

--- src/components/ImageGenerator.jsx
+++ src/components/ImageGenerator.jsx
@@ -1,7 +1,8 @@
 const ImageGenerator = ({ memes }) => {
+  console.log(memes.data.memes[0].url);
   return (
-    <div>
-      <img src="" alt="" />
+    <div className="image-genetaror-container">
+      <img src={memes.data.memes[3].url} alt="" />
     </div>
   );
 };
